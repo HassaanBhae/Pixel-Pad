2fe9126987f7000fdfe12489c06fd2b7e98cacff
HassaanBhae <hassaanmushtaq04@gmail.com>
2024-10-10 02:22:43 +0500

    pre responsive

--- script.js
+++ script.js
@@ -32,98 +32,71 @@ buttons.forEach(button => {
 });
 const colors = [
     // Warm Colors
-    "red",      // Bright red
-    "orange",   // Bright orange
-    "yellow",   // Bright yellow
-    "gold",     // Gold
-    "coral",    // Coral
-    "salmon",   // Salmon
-    "pink",     // Pink
-    "brown",    // Brown
-
+    "red", "orange", "yellow", "gold", "coral", "salmon", "pink", "brown",
     // Cool Colors
-    "green",    // Bright green
-    "lime",     // Lime green
-    "cyan",     // Cyan
-    "blue",     // Bright blue
-    "teal",     // Teal
-    "navy",     // Navy blue
-    "indigo",   // Indigo
-    "violet",   // Violet
-    "purple",   // Purple
-    "magenta",   // Magenta
-
+    "green", "lime", "cyan", "blue", "teal", "navy", "indigo", "violet", "purple", "magenta",
     // Neutrals
-    "white",    // White
-    "gray",     // Gray
-    "black",    // Black
-    "silver",    // Silver
-    "olive",     // Olive
-    "lavender",  // Lavender
-
+    "white", "gray", "black", "silver", "olive", "lavender",
     // Additional Colors
-    "peach",    // Peach
-    "mint",     // Mint green
-    "khaki",    // Khaki
-    "maroon",    // Maroon
-    "turquoise", // Turquoise
-    "charcoal",  // Charcoal gray
-    "periwinkle", // Periwinkle
-    "fuchsia",   // Fuchsia
-    "chartreuse", // Chartreuse
-    "plum",      // Plum
-    "beige",     // Beige
-    "aquamarine" // Aquamarine
+    "peach", "mint", "khaki", "maroon", "turquoise", "charcoal", "periwinkle", "fuchsia", "chartreuse", "plum", "beige", "aquamarine"
 ];
 
 const colorNames = {
-    // Warm Colors
-    red: "rgb(255, 0, 0)",        // Bright red
-    orange: "rgb(255, 165, 0)",   // Bright orange
-    yellow: "rgb(255, 255, 0)",   // Bright yellow
-    gold: "rgb(255, 215, 0)",      // Gold
-    coral: "rgb(255, 127, 80)",    // Coral
-    salmon: "rgb(250, 128, 114)",  // Salmon
-    pink: "rgb(255, 192, 203)",    // Pink
-    brown: "rgb(165, 42, 42)",     // Brown
-
-    // Cool Colors
-    green: "rgb(0, 128, 0)",       // Bright green
-    lime: "rgb(50, 205, 50)",      // Lime green
-    cyan: "rgb(0, 255, 255)",      // Cyan
-    blue: "rgb(0, 0, 255)",        // Bright blue
-    teal: "rgb(0, 128, 128)",      // Teal
-    navy: "rgb(0, 0, 128)",        // Navy blue
-    indigo: "rgb(75, 0, 130)",     // Indigo
-    violet: "rgb(238, 130, 238)",  // Violet
-    purple: "rgb(128, 0, 128)",    // Purple
-    magenta: "rgb(255, 0, 255)",   // Magenta
-
-    // Neutrals
-    white: "rgb(255, 255, 255)",   // White
-    gray: "rgb(128, 128, 128)",     // Gray
-    black: "rgb(0, 0, 0)",           // Black
-    silver: "rgb(192, 192, 192)",    // Silver
-    olive: "rgb(128, 128, 0)",       // Olive
-    lavender: "rgb(230, 230, 250)",  // Lavender
-
-    // Additional Colors
-    peach: "rgb(255, 218, 185)",    // Peach
-    mint: "rgb(189, 252, 201)",     // Mint green
-    khaki: "rgb(240, 230, 140)",    // Khaki
-    maroon: "rgb(128, 0, 0)",        // Maroon
-    turquoise: "rgb(64, 224, 208)",  // Turquoise
-    charcoal: "rgb(54, 69, 79)",     // Charcoal gray
-    periwinkle: "rgb(204, 204, 255)", // Periwinkle
-    fuchsia: "rgb(255, 0, 255)",      // Fuchsia
-    chartreuse: "rgb(127, 255, 0)",   // Chartreuse
-    plum: "rgb(221, 160, 221)",      // Plum
-    beige: "rgb(245, 222, 179)",     // Beige
-    aquamarine: "rgb(127, 255, 212)" // Aquamarine
+    "red": "rgb(255, 0, 0)",
+    "orange": "rgb(255, 165, 0)",
+    "yellow": "rgb(255, 255, 0)",
+    "gold": "rgb(255, 215, 0)",
+    "coral": "rgb(255, 127, 80)",
+    "salmon": "rgb(250, 128, 114)",
+    "pink": "rgb(255, 192, 203)",
+    "brown": "rgb(165, 42, 42)",
+    "green": "rgb(0, 128, 0)",
+    "lime": "rgb(50, 205, 50)",
+    "cyan": "rgb(0, 255, 255)",
+    "blue": "rgb(0, 0, 255)",
+    "teal": "rgb(0, 128, 128)",
+    "navy": "rgb(0, 0, 128)",
+    "indigo": "rgb(75, 0, 130)",
+    "violet": "rgb(238, 130, 238)",
+    "purple": "rgb(128, 0, 128)",
+    "magenta": "rgb(255, 0, 255)",
+    "white": "rgb(255, 255, 255)",
+    "gray": "rgb(128, 128, 128)",
+    "black": "rgb(0, 0, 0)",
+    "silver": "rgb(192, 192, 192)",
+    "olive": "rgb(128, 128, 0)",
+    "lavender": "rgb(230, 230, 250)",
+    "peach": "rgb(255, 218, 185)",
+    "mint": "rgb(189, 252, 201)",
+    "khaki": "rgb(240, 230, 140)",
+    "maroon": "rgb(128, 0, 0)",
+    "turquoise": "rgb(64, 224, 208)",
+    "charcoal": "rgb(54, 69, 79)",
+    "periwinkle": "rgb(204, 204, 255)",
+    "fuchsia": "rgb(255, 0, 255)",
+    "chartreuse": "rgb(127, 255, 0)",
+    "plum": "rgb(221, 160, 221)",
+    "beige": "rgb(245, 222, 179)",
+    "aquamarine": "rgb(127, 255, 212)"
 };
+
+function getColor(index) {
+    if (index < 0 || index >= colors.length) {
+        console.log("Index out of range");
+        return;
+    }
+
+    const colorName = colors[index];
+    const rgbValue = colorNames[colorName] || "Unknown RGB Value";
     
-//To iteterate throughs Colors Array
-let k=0;
+    console.log(`Color Name: ${colorName}, RGB Value: ${rgbValue}`);
+    return rgbValue;
+}
+
+// Example usage
+getColor(0);  // Logs: Color Name: red, RGB Value: rgb(255, 0, 0)
+getColor(10); // Logs: Color Name: cyan, RGB Value: rgb(0, 255, 255)
+
 let currentColor = "white";
 let setColor = "";
 let height = "50px";
@@ -187,15 +160,49 @@ function resetf(){
     });
     console.log("resetcalled")
 };
-
 function draw(height,width,total){
+    let opacity = 0.4;
     for (let i=0;i<total;i++){
         const div = document.createElement("div");
         div.style.width=width;
         div.style.height=height;
+        div.style.opacity = opacity;
         div.classList.add("divs");
+        div.dataset.opacitycount=0;
         div.addEventListener("click", () => {
             div.style.backgroundColor = currentColor; // Change color on hover
+            // if(div.dataset.setColor === currentColor){
+            //     opacity = opacity+0.2;
+            //     div.style.opacity = opacity;
+            //     console.log(opacity, "Opacity Set","==Opacity Count",div.dataset.opacitycount);
+            // }else{
+            //     opacity = 0.4;
+            //     div.style.opacity = opacity;
+            //     console.log(opacity, "Opacity ReSet");
+            // }
+            let count = Number(div.dataset.opacitycount);
+            switch(count){
+                case 0:
+                    console.log('case 0');
+                    opacity=0.4;
+                    break;
+                case 1:
+                    console.log('case 1');
+                    opacity=0.6;
+                    break;
+                case 2:
+                    console.log('case 2');
+                    opacity=0.8;
+                    break;
+                case 3:
+                    console.log('case 3');
+                    opacity=1;
+                    break;
+                default:
+                    break;
+            }
+            div.style.opacity = opacity;
+            div.dataset.opacitycount++;
             div.dataset.setColor = currentColor;
         });   
         div.addEventListener("mouseover", () => {
@@ -217,13 +224,14 @@ function deleteGrid(){
 for (let j=0;j<36;j++){
     const colorsDiv = document.createElement("div");
     colorsDiv.classList.add("colors");
-    colorsDiv.style.backgroundColor = (colors[k]);   
+    colorsDiv.style.backgroundColor = getColor(j);   
     colorsDiv.addEventListener("click", () => {
         currentColor = getComputedStyle(colorsDiv).backgroundColor;
-        const colorName = colorNames[currentColor] || "Unknown Color";
-        console.log(colorName);
+        // const colorName = colorNames[currentColor] || "Unknown Color";
+        // console.log(colorName);
     });
-    k++;
+    console.log(j);
+    console.log(colors[j]);
     colorPallete.appendChild(colorsDiv);     
 }
 
